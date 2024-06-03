6ed63a9b280d9f8998dcd355a7bf61077a3e1c6e
s27920 <janekkubisa8@gmail.com>
2024-06-03 02:57:48 +0200

    idk man

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -5,7 +5,7 @@ let extendedDiv;
 let extendedImg;
 let placeHolderDiv;
 
-function insertElement(restName, rating, address){
+function insertElement(restName, rating, address, website, priceRange, zipCode){
     let path = "stars/"+ setRating(rating) + ".png";
     let imgPath = "images/"+restName+".jpg";
 
@@ -13,6 +13,7 @@ function insertElement(restName, rating, address){
     newDiv.style.textAlign = "left";
     newDiv.style.color = "rgb(85, 85, 85)";
     newDiv.className = "enumerate";
+    newDiv.style.overflow = "hidden";
 
     const img = document.createElement("div");
     img.style.display = "flex";
@@ -41,22 +42,57 @@ function insertElement(restName, rating, address){
     starRating.style.marginTop = "-20px";
     newDiv.append(starRating);
 
+    let resizedPar = document.createElement("div");
+
     let par = document.createElement("p");
     par.innerText = address;
     par.className = "tileText";
     newDiv.append(par);
 
+    let parHidden = document.createElement("p");
+    parHidden.innerText =  zipCode.substring(0,2) + "-" + zipCode.substring(2,zipCode.length+1)+ "\n" +priceRange;
+    parHidden.className = "tileText";
+
+    let mapDiv = document.createElement("div");
+    let mapImg = document.createElement("img");
+    mapImg.src = "src/main/webapp/images/placeholder.png";
+    mapDiv.style.float = "right";
+    let mapUpdateButton = document.createElement("p");
+    mapUpdateButton.innerText = "See on map";
+    mapUpdateButton.style.textAlign = "left";
+    mapUpdateButton.style.fontFamily = "'Cabin', Verdana, serif";
+    mapUpdateButton.style.marginTop = "0";
+    mapUpdateButton.style.marginLeft = "15px";
+    mapUpdateButton.style.marginBottom = "5px";
+    mapUpdateButton.style.color = "#3a0b9e";
+    mapUpdateButton.style.textDecoration = "underline";
+    mapUpdateButton.addEventListener("click", ()=>{
+        //hardcoded for testing TODO remove later
+        updateMap("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d215.99793089017265!2d20.994179630340053!3d52.22998447352774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc85d2f64dc3%3A0xe35f891e106337b4!2sMiss%20Kimchi!5e0!3m2!1spl!2spl!4v1717365718913!5m2!1spl!2spl");
+    })
+    mapDiv.append(mapImg);
+    mapDiv.append(mapUpdateButton);
+//TODO pls finish
+    resizedPar.append(parHidden);
+    resizedPar.append(mapDiv);
+    resizedPar.style.visibility = "hidden";
+    newDiv.append(resizedPar);
+
+
     const newLi = document.createElement("li");
     newLi.append(newDiv);
     let target = document.getElementById("enumerate-list");
     target.append(newLi);
-    console.log(newDiv.style.left);
     if(checkListScrollability()){
         turnButtonsOffOn("visible");
     }
     newDiv.addEventListener("click", ()=>{
         toggleSize(newDiv, imgProper, "230px", "345px", "200px", "300px", "196px", "288px");
     }, {passive: true});
+}
+
+function updateMap(link){
+    document.getElementById("map").src=link;
 }
 
 function toggleSize(newDiv, imgProper, nh1, nh2, nw1, nw2, iw1, iw2){
@@ -79,14 +115,15 @@ function toggleSize(newDiv, imgProper, nh1, nh2, nw1, nw2, iw1, iw2){
             newDiv.parentNode.append(clonedDiv);
         }
         newDiv.style.left = newDiv.parentNode.getBoundingClientRect().left + "px"
-        console.log(newDiv.style.left);
         newDiv.style.position = "absolute";
         newDiv.style.zIndex = '10';
+        toggleVisibleDic(newDiv, "visible")
         imgProper.style.width = iw2;
         newDiv.style.height = nh2;
         newDiv.style.width = nw2;
         newDiv.style.transform = "translate(" + slide +"px, 0px)"
     }else{
+        toggleVisibleDic(newDiv, "hidden")
         imgProper.style.width = iw1;
         newDiv.style.height = nh1;
         newDiv.style.width = nw1;
@@ -107,6 +144,11 @@ function toggleSize(newDiv, imgProper, nh1, nh2, nw1, nw2, iw1, iw2){
         extendedDiv = newDiv;
         extendedImg = imgProper;
     }
+}
+
+function toggleVisibleDic(div, visible){
+    const children = Array.from(div.children);
+    children.at(children.length - 1).style.visibility = visible;
 }
 
 function setRating(textRating){
@@ -220,7 +262,7 @@ function search(){
         .then(result => {
             removeAllElements();
             result.forEach(restaurant =>{
-                insertElement(restaurant.name, restaurant.rating, restaurant.address);
+                insertElement(restaurant.name, restaurant.rating, restaurant.address, restaurant.website, restaurant.priceRange, restaurant.zipCode);
             });
             console.log('Success:', result);
         })
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -103,7 +103,7 @@
         </div>
     </div>
     <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13821.795415809722!2d21.049423755571517!3d52.23663889053836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1717341531809!5m2!1spl!2spl" width="800" height="450" style="border:0;z-index: 5;position: relative;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    </p>
+        <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13821.795415809722!2d21.049423755571517!3d52.23663889053836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1717341531809!5m2!1spl!2spl" width="800" height="600" style="border:0;z-index: 5;position: relative;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
 
